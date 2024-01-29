c163cf4e8db72c83e63db4f0eb1d07df41021cf8
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-01-29 18:09:25 +0530

    register routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+
+import bodyParser from 'body-parser'
 
 const PORT = process.env.PORT || 7000
 mongoose.connect(process.env.MONGODB_CONNEXTION_STRING as string);
@@ -10,10 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/api/test", async(req: Request, res: Response) => {
-  res.json({message: "Hello from express endpoint"})
-});
+app.use("/api/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on ${PORT} `)
