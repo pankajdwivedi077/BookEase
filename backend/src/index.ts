b428b2e3ab7d1658405e139d65d059c2d3caf4e8
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-01-30 14:17:47 +0530

    login routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
 
 import bodyParser from 'body-parser'
 
@@ -15,7 +16,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on ${PORT} `)
