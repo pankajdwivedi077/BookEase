20e141a1bd08b575e2385bc9fa0eb872c89ff7b6
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-01-31 14:27:11 +0530

    sign in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
@@ -19,14 +20,30 @@ function App() {
             </Layout>
           }
         />
-        <Route path="/search" element={
-           <Layout>
-           <p>Search Page</p>
-         </Layout>
-        } />
-        <Route path="/register" element={ <Layout>
-          <Register />
-        </Layout> } />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <Signin />
+            </Layout>
+          }
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
