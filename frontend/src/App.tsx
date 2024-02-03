42b66817ab38cb55782b136368ea1c8ac6b8f42a
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-03 16:34:46 +0530

    searchBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContest";
 import MyHotel from "./pages/MyHotel";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -29,7 +30,7 @@ function App() {
           path="/search"
           element={
             <Layout>
-              <p>Search Page</p>
+              <Search />
             </Layout>
           }
         />
